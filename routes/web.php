2332f9dfdd77cca77ba9e3308cf5900712948a1b
c-nakajima <test@example.com>
2023-05-24 16:22:51 +0000

    課題08

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,20 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-   Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
+
+// 課題08-3 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('profile/edit', 'edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile/create', 'add')->name('profile.add');
+// 課題08-6
+    Route::post('profile/edit', 'update')->name('profile.update');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
 });
 Auth::routes();
 
